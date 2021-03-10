6fa47bda83f69d3611975cec5c970c1f0afe40d5
Marián Puškáš <36486631+Marian96@users.noreply.github.com>
2021-03-10 10:46:59 +0100

    first

---
+++ projects/Inspector/www/pbi/PowerBiOAuthSettings.js
@@ -0,0 +1,15 @@
+﻿/** PowerBi OAuth settings */
+var PowerBiOAuthSettings = {
+    /**ClientId of the application the token was issued for. */
+    client_id: '3c661148-c7b8-4859-8c29-0d2096533bcc',
+    /**Authentication clientSecret object. */
+    client_secret: 'MgNOb8FVFnlJaXNUzRBJykOxTwAwNQEnqdxjxDP+ZG0=',
+    /**Dashboard id what will be displayed. (Can be taken from url of dashboard). */
+    dashboardId: '',
+    /**Report id what will be displayed. (Can be taken from url of report). */
+    reportId: '80b11f89-c2e2-477c-abca-e42a8a654eff',
+    /** Id Of workspace what contains Dashboard and Report. */
+    workspaceGroupId: 'fe00034e-90ef-43e0-9f5e-0838e6869184',
+    /** Authorization redirect url for service. */
+    redirectUrl: 'https://www.resco.net/oauth.html',
+};
