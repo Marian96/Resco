4ead50b4c7334ff38d1663686d3642d86c05bf93
Marián Puškáš <36486631+Marian96@users.noreply.github.com>
2021-03-10 10:47:51 +0100

    Manager commit

---
+++ projects/Manager/www/precache-manifest.7ccc2717fd934d972cc51866c67f9924.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f123b104297ac5cee13d5b0bde61d9d9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3fa436190bb489a0dfb3",
+    "url": "./static/css/main.1ee50553.chunk.css"
+  },
+  {
+    "revision": "c28f0a382b382d0f9aa8",
+    "url": "./static/js/2.0000d243.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.0000d243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa436190bb489a0dfb3",
+    "url": "./static/js/main.8a06034b.chunk.js"
+  },
+  {
+    "revision": "ccfa0b99d09a6abdc911",
+    "url": "./static/js/runtime-main.97b93fc4.js"
+  }
+]);
